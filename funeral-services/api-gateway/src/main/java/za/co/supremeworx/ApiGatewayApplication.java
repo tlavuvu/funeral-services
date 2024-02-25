d97702d88cd61be180e5e04d8fab8682773cde61
vince <vince@host.docker.internal>
2024-02-25 02:10:42 +0200

    after configuring api-gateway security

---
+++ funeral-services/api-gateway/src/main/java/za/co/supremeworx/ApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package za.co.supremeworx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
