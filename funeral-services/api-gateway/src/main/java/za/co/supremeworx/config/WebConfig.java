25abe265d5b68866c71ac3bfab3c78a65e8dd481
vince <vince@host.docker.internal>
2024-03-05 15:18:48 +0200

    latest

---
+++ funeral-services/api-gateway/src/main/java/za/co/supremeworx/config/WebConfig.java
@@ -0,0 +1,20 @@
+//package za.co.supremeworx.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import za.co.supremeworx.interceptors.LogHandlerInterceptor;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer{
+//
+//	@Override
+//	public void addInterceptors(InterceptorRegistry registry) {
+//		// TODO Auto-generated method stub
+////		WebMvcConfigurer.super.addInterceptors(registry);
+//		registry.addInterceptor(new LogHandlerInterceptor());
+//	}
+//
+//	
+//}
---
+++ funeral-services/notification-service/src/main/java/za/co/supremeworx/interceptors/LogHandlerInterceptor.java
@@ -0,0 +1,39 @@
+package za.co.supremeworx.interceptors;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LogHandlerInterceptor implements HandlerInterceptor{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+//		return HandlerInterceptor.super.preHandle(request, response, handler);
+		log.info("LogHandlerInterceptor::prehandle {} {}",request.getLocalAddr(),request.getHeaderNames());
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		log.info("LogHandlerInterceptor::postHandle");
+		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		log.info("LogHandlerInterceptor::afterCompletion");
+		HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+	}
+
+	
+}
